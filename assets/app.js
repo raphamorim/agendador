d5476990c5cd70f414c15e2e8345bd94a05724c8
Raphael Amorim <rapha850@gmail.com>
2015-09-28 16:52:04 -0300

    update

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,32 @@
 
 // Estúdio disponíveis
-// Usar um modal e da show nele
 // Função de login
 
-var busyDates = [1, 3, 14, 24];
+var busyDates = [1, 3, 14, 24],
+    session = false,
+    theDay = false;
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function getSession() {
+    if (getCookie('email'))
+        session = true;
+    else 
+        session = false;
+}
+
 (function(){
+    getSession();
+
     var today = document.querySelector('.current-day'),
         // today = new Date(),
         calendar = document.querySelectorAll('.calendar tbody tr td');
@@ -25,9 +47,44 @@ var busyDates = [1, 3, 14, 24];
                 if (date <= today)
                     data.innerHTML = '<h2>Inválido</h2><p>Por favor coloque um data válida!</p>';
                 else if (busyDates.indexOf(date) > -1)
-                    data.innerHTML = '<h2>Indisponível</h2><p>Esta data não possui disponível!</p>'
-                else 
-                    data.innerHTML = '<h2>Data disponível</h2><p></p>';
+                    data.innerHTML = '<h2>Indisponível</h2><p>Esta data não possui estúdio disponível!</p>'
+                else  {
+                    theDay = date;
+                    if (!session) {
+                        data.innerHTML = '<h2>Data disponível</h2><p><a href="#" class="signup">Cadastre-se!</a></p><p><a href="#" class="signin">Login</a></p>';
+                        document.querySelector('.signup').addEventListener('click', function() {
+                            var c = document.querySelector('.container');
+                            c.innerHTML = '<h1>Cadastre-se!</h1><h4>Para se cadastrar é fácil e rápido!</h4>' + 
+                            '<p><input type="text" class="nome" required placeholder="Nome"/></p>' + 
+                            '<p><input type="email" class="email" required placeholder="Email"/></p>' +
+                            '<p><input type="password" class="senha" required placeholder="Senha"/></p>' +
+                            '<p><a href="#" class="signup-btn">Cadastre-se</a></p>';
+                            document.querySelector('.signup-btn').addEventListener('click', function() {
+                                var cookie = "email=" + document.querySelector('.email').value + 
+                                    "; senha=" + document.querySelector('.senha').value +
+                                    "; nome=" + document.querySelector('.nome').value;
+                                document.cookie = cookie;
+                                location.reload();
+                            });
+                        });
+                        document.querySelector('.signin').addEventListener('click', function() {
+                            var c = document.querySelector('.container');
+                            c.innerHTML = '<h1>Login</h1><h4>Entre no Studion</h4>';                            
+                        });
+                    } else {
+                        data.innerHTML = '<h2>Escolher estúdio</h2>' + 
+                          '<p><a href="#" class="studio-option">AudioRebel</a></p>' +
+                          '<p><a href="#" class="studio-option">Hanoi</a></p>';
+                        var estudios = document.querySelectorAll('.studio-option');
+                        for (var i = 0; i < estudios.length; i++) {
+                            var estudio = (estudios[i].innerText || estudios[i].textContent);
+                            estudios[i].addEventListener('click', function() {
+                                var c = document.querySelector('.container');
+                                c.innerHTML = '<h1>Parabéns!</h1><h4>Você agendou com ' + estudio + '. Aguarde para ter mais informações!</h4>';                            
+                            });
+                        }
+                    }
+                }
 
                 var evt = document.createEvent("MouseEvents");
                     evt.initMouseEvent("click", true, true, window,
